144118f65d6554abc643a82290c6d1f093bad869
YoungFish <im.young@foxmail.com>
2023-10-29 11:19:34 +0800

    test

--- src/main.py
+++ src/main.py
@@ -43,34 +43,35 @@ class Scaffold:
             except (requests.HTTPError, requests.exceptions.RequestException, json.JSONDecodeError) as err:  
                 logger.error(f'Error occurred: {err}')  
                 continue 
-            logger.info(f"dblp_data: {dblp_data}")
+            else:
+                logger.info(f"dblp_data: {dblp_data}")
 
-            # get items
-            items = get_dblp_items(dblp_data)
-            logger.info(f"items: {items}")
+                # get items
+                items = get_dblp_items(dblp_data)
+                logger.info(f"items: {items}")
 
-            # new cache
-            cached_items = dblp_cache.get(topic, [])
-            new_items = [item for item in items if item not in cached_items]
-            dblp_new_cache[topic] = new_items
+                # new cache
+                cached_items = dblp_cache.get(topic, [])
+                new_items = [item for item in items if item not in cached_items]
+                dblp_new_cache[topic] = new_items
 
-            if topic not in dblp_cache:
-                dblp_cache[topic] = []
-            dblp_cache[topic].extend(new_items)
+                if topic not in dblp_cache:
+                    dblp_cache[topic] = []
+                dblp_cache[topic].extend(new_items)
 
-            logger.info(f"new_items: {new_items}")
+                logger.info(f"new_items: {new_items}")
 
 
-            # if there is any new items, we set flag to create a new issue
-            if len(new_items) > 0:
-                flag = True
-            
-            # only when new items >0 in this topic we creat the msg
-            if len(new_items) > 0:
-                aggregated_msg+= get_msg(new_items, topic ,True)
-                msg += get_msg(new_items, topic)
-            logger.info(f"aggregated_msg: {aggregated_msg}")
-            logger.info(f"msg: {msg}")
+                # if there is any new items, we set flag to create a new issue
+                if len(new_items) > 0:
+                    flag = True
+                
+                # only when new items >0 in this topic we creat the msg
+                if len(new_items) > 0:
+                    aggregated_msg+= get_msg(new_items, topic ,True)
+                    msg += get_msg(new_items, topic)
+                logger.info(f"aggregated_msg: {aggregated_msg}")
+                logger.info(f"msg: {msg}")
 
         # save cache
         yaml.safe_dump(dblp_cache, open(cache_path, "w"), sort_keys=False, indent=2)
